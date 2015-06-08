8b3f8fbd0dd698eb1334bf5311cfdfcfd568d7f6
kyu999 <kyukokkyou999@gmail.com>
2015-06-08 17:59:07 +0900

    fix bug to create wordcloud

--- app.js
+++ app.js
@@ -1,36 +1,44 @@
 
-  var fill = d3.scale.category20();
+var fill = d3.scale.category20();
 
-  function makeWordCloud(wordfreqs){
-    var sizeScale = d3.scale.linear().domain([0, 5]).range([10, 100])
+  var makeWordCloud = function(wordfreqs, parent_elem, svgscale){
 
-    d3.layout.cloud().size([300, 300])
+      d3.select("svg").remove()
+
+      var freq_max =  d3.max(wordfreqs, function(wf){ return wf.freq } );
+      var sizeScale = d3.scale.linear().domain([0, freq_max]).range([0, 1])
+
+      wordfreqs = wordfreqs.map(function(d) {
+        return {text: d.word, size: 10 + sizeScale(d.freq) * 90};
+      })
+
+      d3.layout.cloud().size([svgscale, svgscale])
       .words(wordfreqs)
       .padding(5)
       .rotate(function() { return ~~(Math.random() * 2) * 90; })
       .font("Impact")
-      .fontSize(function(d) { return sizeScale(d.freq); })
+      .fontSize(function(d) { return d.size; })
       .on("end", draw)
       .start();
-  }
-  function draw(words) {
-    d3.select("svg").remove()
-    d3.select("#notebook").append("svg")
-        .attr("width", 500)
-        .attr("height", 300)
-      .append("g")
-        .attr("transform", "translate(150,150)")
-      .selectAll("text")
-        .data(words)
-      .enter().append("text")
-        .style("font-size", function(d) { return d.size + "px"; })
-        .style("font-family", "Impact")
-        .style("fill", function(d, i) { return fill(i); })
-        .attr("text-anchor", "middle")
-        .attr("transform", function(d) {
-          return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
-        })
-        .text(function(d) { return d.word; });
+
+      function draw(words) {
+        d3.select(parent_elem).append("svg")
+            .attr("width", svgscale)
+            .attr("height", svgscale)
+          .append("g")
+            .attr("transform", "translate(" + svgscale / 2 + "," + svgscale / 2 + ")")
+          .selectAll("text")
+            .data(words)
+          .enter().append("text")
+            .style("font-size", function(d) { return d.size + "px"; })
+            .style("font-family", "Impact")
+            .style("fill", function(d, i) { return fill(i); })
+            .attr("text-anchor", "middle")
+            .attr("transform", function(d) {
+              return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+            })
+            .text(function(d) { return d.text; });
+      }
   }
 
 
@@ -44,6 +52,10 @@ var compare = function(dic1, dic2){
   return dic1.freq - dic2.freq;
 }
 
+// 有効な品詞群
+var validFeature = ["E", "N-n", "N-nc", "N-pn", "V-c", "F", "J-c", "J-tari"]
+console.log(validFeature)
+
 // ユーザーがコピペしたテキストを取得する(angularを利用して瞬時に取得する)
 // 取得したテキストを形態素解析にかける(token化)
 var check_wordfreq = function(tokens){
@@ -53,13 +65,19 @@ var check_wordfreq = function(tokens){
     tokens.forEach(function(token){
 
         word = token[0]
+        feature = token[1]
 
-        if(word in wordfreq){
-            wordfreq[word] = wordfreq[word] + 1
+        // 前置詞など不要な単語はカウントせず
+        if($.inArray(feature, validFeature) >= 0){
+
+          if(word in wordfreq){
+              wordfreq[word] = wordfreq[word] + 1
+          }
+          else{
+              wordfreq[word] = 1
+          }
         }
-        else{
-            wordfreq[word] = 1
-        }
+
     })
 
     // Hashmap => Sorted Array by Word Frequency
@@ -87,26 +105,19 @@ var mainCtrl = function($scope) {
     $scope.wordcount = 0
     $scope.wordfreq = []
 
-    //ユーザーがコピペしたテキストを取得する(angularを利用して瞬時に取得する)
+    //ユーザーがコピペしたテキストが変更されるたびに実行
     $scope.$watch("text", function(newValue, oldValue){
         $scope.wordcount = newValue.length
         $scope.tokens = rma.tokenize(newValue)
         $scope.text = newValue
         $scope.wordfreq = check_wordfreq($scope.tokens)
-        console.log($scope.wordfreq)
-        makeWordCloud($scope.wordfreq)
-    });
 
-    $scope.analyze = function(){
-        
-    }
+        makeWordCloud($scope.wordfreq, "#notebook", 500)
+    });
 
     $scope.clear = function(){
         $scope.text = ""
     }
 
 }
-
-// wordcloud
-
   
