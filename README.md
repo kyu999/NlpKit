b1f708c6fafd10496c154998d3728c41db7455b3
kyu999 <kyukokkyou999@gmail.com>
2015-06-08 19:25:01 +0900

    done

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# NlpKit
+# EasyWordCloud
 
-Welcome to NlpKit. It's a Japanese text analyzer on browser. You don't need to install anything. Just copy and paste your text.
+- How to use
--- wordclouding.js
+++ wordclouding.js
@@ -2,7 +2,7 @@
 var fill = d3.scale.category20();
 
 // windowオブジェクトのプロパティにすることでmakeWordCloudを他のファイルから呼び出せるようにする
-window.makeWordCloud = function(wordfreqs, parent_elem, svgscale){
+window.makeWordCloud = function(data, parent_elem, svgscale){
 
       function draw(words) {
         d3.select(parent_elem).append("svg")
@@ -25,15 +25,15 @@ window.makeWordCloud = function(wordfreqs, parent_elem, svgscale){
 
       d3.select("svg").remove()
 
-      var freq_max =  d3.max(wordfreqs, function(wf){ return wf.freq } );
-      var sizeScale = d3.scale.linear().domain([0, freq_max]).range([0, 1])
+      var data_max =  d3.max(data, function(d){ return d.value } );
+      var sizeScale = d3.scale.linear().domain([0, data_max]).range([0, 1])
 
-      wordfreqs = wordfreqs.map(function(d) {
-        return {text: d.word, size: 10 + sizeScale(d.freq) * 90};
+      data = data.map(function(d) {
+        return {text: d.word, size: 10 + sizeScale(d.value) * 90};
       })
 
       d3.layout.cloud().size([svgscale, svgscale])
-        .words(wordfreqs)
+        .words(data)
         .padding(5)
         .rotate(function() { return ~~(Math.random() * 2) * 90; })
         .font("Impact")
