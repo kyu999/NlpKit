95aed7baf3758ed675bfa6f5050b67ce9f4728a8
kyu999 <kyukokkyou999@gmail.com>
2015-06-19 21:56:19 +0900

    fix bug

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-#!/usr/bin/env  python2.7
 from flask import Flask, jsonify
-#from nlp import NlpKit
+from nlp import NlpKit
 
 app = Flask(__name__)
 app.debug=True
@@ -14,9 +13,9 @@ def hello_world():
 @app.route('/nlp/<text>')
 def nlp(text):
   result = {
-    "data": text#kit.analyze(text, "data/wakati_words.txt")
+    "data": kit.analyze(text, "data/wakati_words.txt")
   }
   return jsonify(result)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run()
